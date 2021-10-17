5c88520fd8b1f635c7fc78dc3bcaa641e88cca0b
PhilGW <philip.g.wagner@gmail.com>
2021-10-16 21:24:42 -0600

    Updated settings.py

--- mystelleri/settings.py
+++ mystelleri/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import mimetypes
+from decouple import config
 
 mimetypes.add_type("text/css", ".css", True)
 
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(@yi@7q(yc6s71@uk+it%3$6cjiqgm$*o#0c-)lc5r^kdui#v6'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
